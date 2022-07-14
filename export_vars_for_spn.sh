0592cadb6e2f30eafa1350d1ff7ea6748571b505
AnnaDobrycheva <38880804+AnnaDob@users.noreply.github.com>
2022-07-14 17:48:03 +0300

    Update export_vars_for_spn.sh

--- export_vars_for_spn.sh
+++ export_vars_for_spn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ARM_SUBSCRIPTION_ID="dd568595-5cd6-440e-bac6-3e52bc422ed6"
-export ARM_CLIENT_ID="773e99f6-3bb0-4587-b5ac-bd061bd3ad95"
-export ARM_TENANT_ID="03586d25-90dc-41cd-be8c-e9d2d76d150c"
-export ARM_CLIENT_SECRET="-8A8Q~-6TYvhlFwqbdShwexzTXZ8CHT~09DMRay0"
+export ARM_SUBSCRIPTION_ID="XXX"
+export ARM_CLIENT_ID="XXX"
+export ARM_TENANT_ID="XXX"
+export ARM_CLIENT_SECRET="XXX"
